6ff64ae995c09f89130a288e812c37ae325ae1a8
animeshpatnaik <86304231+animeshpatnaik@users.noreply.github.com>
2021-06-23 20:18:42 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+s = """I am
+ Grateful"""
+print(s)
+print("Printing the second letter of string: "+s[2])
+
+##Printing the string twice
+print(s*2)
+
+#length of string
+
+print("length of string is:"+str(len(s)))
